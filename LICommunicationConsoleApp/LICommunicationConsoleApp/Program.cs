658ea3c92ba4cfa0ffdd46db7a81d47f3a19e6e9
Zachary <zachary.c.hall@wsu.edu>
2023-06-01 06:06:02 -0700

    Added serial port config. Added console output and input.

---
+++ LICommunicationConsoleApp/LICommunicationConsoleApp/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+namespace LICommunicationConsoleApp
+{
+    using System;
+    using System.IO.Ports;
+    public class Program
+    {
+        public static void Main()
+        {
+            SerialPort serialPort = new SerialPort("COM1", 9600, Parity.None, 8, StopBits.One); // Name, Baudrate, Parity, Databits, Stopbits
+
+            try
+            {
+                serialPort.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            string command;
+            string result;
+
+            do
+            {
+                Console.WriteLine("Enter 'quit' to quit.");
+                Console.WriteLine("Enter command for LI-7000: ");
+
+                command = Console.ReadLine();
+
+                if (command != "" && command != "quit" && serialPort.IsOpen)
+                {
+                    serialPort.WriteLine(command);
+                    result = serialPort.ReadExisting();
+
+                    Console.WriteLine("LI-7000 responded: ");
+                    Console.WriteLine(result);
+                }
+
+
+            } while (command != "quit" && command != "");
+
+            serialPort.Close();
+
+        }
+    }
+}
