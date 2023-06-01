1b78896d1204bf99991d557bf5c6335d14a9490a
Zachary <zachary.c.hall@wsu.edu>
2023-05-31 20:14:43 -0700

    Added base UI elements.

--- ProResp/ProResp/Form1.Designer.cs
+++ ProResp/ProResp/Form1.Designer.cs
@@ -28,12 +28,217 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.CheckAllValves_Button = new System.Windows.Forms.Button();
+            this.StartNewExperiment_Button = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.ValveChecklist_Label = new System.Windows.Forms.Label();
+            this.Stop_Button = new System.Windows.Forms.Button();
+            this.ActiveChamber_Label = new System.Windows.Forms.Label();
+            this.CurrentCO2_Label = new System.Windows.Forms.Label();
+            this.CurrentH2O_Label = new System.Windows.Forms.Label();
+            this.CurrentTemp_Label = new System.Windows.Forms.Label();
+            this.CurrentFlow_Label = new System.Windows.Forms.Label();
+            this.CurrentFileLocation_Label = new System.Windows.Forms.Label();
+            this.PreviousValve_Label = new System.Windows.Forms.Label();
+            this.FinalCO2_Label = new System.Windows.Forms.Label();
+            this.FinalH2O_Label = new System.Windows.Forms.Label();
+            this.FinalTemp_Label = new System.Windows.Forms.Label();
+            this.FinalFlow_Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CheckAllValves_Button
+            // 
+            this.CheckAllValves_Button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.CheckAllValves_Button.Location = new System.Drawing.Point(54, 93);
+            this.CheckAllValves_Button.Name = "CheckAllValves_Button";
+            this.CheckAllValves_Button.Size = new System.Drawing.Size(253, 141);
+            this.CheckAllValves_Button.TabIndex = 0;
+            this.CheckAllValves_Button.Text = "Check All Valves";
+            this.CheckAllValves_Button.UseVisualStyleBackColor = true;
+            this.CheckAllValves_Button.Click += new System.EventHandler(this.CheckAllValves_Button_Click);
+            // 
+            // StartNewExperiment_Button
+            // 
+            this.StartNewExperiment_Button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.StartNewExperiment_Button.Location = new System.Drawing.Point(54, 438);
+            this.StartNewExperiment_Button.Name = "StartNewExperiment_Button";
+            this.StartNewExperiment_Button.Size = new System.Drawing.Size(253, 141);
+            this.StartNewExperiment_Button.TabIndex = 1;
+            this.StartNewExperiment_Button.Text = "Start New Experiment";
+            this.StartNewExperiment_Button.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(374, 121);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(271, 508);
+            this.checkedListBox1.TabIndex = 2;
+            // 
+            // ValveChecklist_Label
+            // 
+            this.ValveChecklist_Label.Location = new System.Drawing.Point(374, 38);
+            this.ValveChecklist_Label.Name = "ValveChecklist_Label";
+            this.ValveChecklist_Label.Size = new System.Drawing.Size(269, 66);
+            this.ValveChecklist_Label.TabIndex = 3;
+            this.ValveChecklist_Label.Text = "Select Valves For Experiment";
+            this.ValveChecklist_Label.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.ValveChecklist_Label.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Stop_Button
+            // 
+            this.Stop_Button.Location = new System.Drawing.Point(726, 143);
+            this.Stop_Button.Name = "Stop_Button";
+            this.Stop_Button.Size = new System.Drawing.Size(217, 413);
+            this.Stop_Button.TabIndex = 4;
+            this.Stop_Button.Text = "Stop";
+            this.Stop_Button.UseVisualStyleBackColor = true;
+            // 
+            // ActiveChamber_Label
+            // 
+            this.ActiveChamber_Label.AutoSize = true;
+            this.ActiveChamber_Label.Location = new System.Drawing.Point(1043, 115);
+            this.ActiveChamber_Label.Name = "ActiveChamber_Label";
+            this.ActiveChamber_Label.Size = new System.Drawing.Size(154, 32);
+            this.ActiveChamber_Label.TabIndex = 5;
+            this.ActiveChamber_Label.Text = "Active Valve: ";
+            this.ActiveChamber_Label.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // CurrentCO2_Label
+            // 
+            this.CurrentCO2_Label.AutoSize = true;
+            this.CurrentCO2_Label.Location = new System.Drawing.Point(1040, 200);
+            this.CurrentCO2_Label.Name = "CurrentCO2_Label";
+            this.CurrentCO2_Label.Size = new System.Drawing.Size(151, 32);
+            this.CurrentCO2_Label.TabIndex = 6;
+            this.CurrentCO2_Label.Text = "Current CO2:";
+            // 
+            // CurrentH2O_Label
+            // 
+            this.CurrentH2O_Label.AutoSize = true;
+            this.CurrentH2O_Label.Location = new System.Drawing.Point(1042, 294);
+            this.CurrentH2O_Label.Name = "CurrentH2O_Label";
+            this.CurrentH2O_Label.Size = new System.Drawing.Size(154, 32);
+            this.CurrentH2O_Label.TabIndex = 7;
+            this.CurrentH2O_Label.Text = "Current H2O:";
+            // 
+            // CurrentTemp_Label
+            // 
+            this.CurrentTemp_Label.AutoSize = true;
+            this.CurrentTemp_Label.Location = new System.Drawing.Point(1047, 382);
+            this.CurrentTemp_Label.Name = "CurrentTemp_Label";
+            this.CurrentTemp_Label.Size = new System.Drawing.Size(165, 32);
+            this.CurrentTemp_Label.TabIndex = 8;
+            this.CurrentTemp_Label.Text = "Current Temp:";
+            // 
+            // CurrentFlow_Label
+            // 
+            this.CurrentFlow_Label.AutoSize = true;
+            this.CurrentFlow_Label.Location = new System.Drawing.Point(1045, 456);
+            this.CurrentFlow_Label.Name = "CurrentFlow_Label";
+            this.CurrentFlow_Label.Size = new System.Drawing.Size(155, 32);
+            this.CurrentFlow_Label.TabIndex = 9;
+            this.CurrentFlow_Label.Text = "Current Flow:";
+            // 
+            // CurrentFileLocation_Label
+            // 
+            this.CurrentFileLocation_Label.AutoSize = true;
+            this.CurrentFileLocation_Label.Location = new System.Drawing.Point(1050, 579);
+            this.CurrentFileLocation_Label.Name = "CurrentFileLocation_Label";
+            this.CurrentFileLocation_Label.Size = new System.Drawing.Size(247, 32);
+            this.CurrentFileLocation_Label.TabIndex = 10;
+            this.CurrentFileLocation_Label.Text = "Current File Location: ";
+            // 
+            // PreviousValve_Label
+            // 
+            this.PreviousValve_Label.AutoSize = true;
+            this.PreviousValve_Label.Location = new System.Drawing.Point(1321, 115);
+            this.PreviousValve_Label.Name = "PreviousValve_Label";
+            this.PreviousValve_Label.Size = new System.Drawing.Size(172, 32);
+            this.PreviousValve_Label.TabIndex = 11;
+            this.PreviousValve_Label.Text = "Previous Valve:";
+            // 
+            // FinalCO2_Label
+            // 
+            this.FinalCO2_Label.AutoSize = true;
+            this.FinalCO2_Label.Location = new System.Drawing.Point(1319, 196);
+            this.FinalCO2_Label.Name = "FinalCO2_Label";
+            this.FinalCO2_Label.Size = new System.Drawing.Size(121, 32);
+            this.FinalCO2_Label.TabIndex = 12;
+            this.FinalCO2_Label.Text = "Final CO2:";
+            // 
+            // FinalH2O_Label
+            // 
+            this.FinalH2O_Label.AutoSize = true;
+            this.FinalH2O_Label.Location = new System.Drawing.Point(1325, 299);
+            this.FinalH2O_Label.Name = "FinalH2O_Label";
+            this.FinalH2O_Label.Size = new System.Drawing.Size(131, 32);
+            this.FinalH2O_Label.TabIndex = 13;
+            this.FinalH2O_Label.Text = "Final H2O: ";
+            // 
+            // FinalTemp_Label
+            // 
+            this.FinalTemp_Label.AutoSize = true;
+            this.FinalTemp_Label.Location = new System.Drawing.Point(1328, 390);
+            this.FinalTemp_Label.Name = "FinalTemp_Label";
+            this.FinalTemp_Label.Size = new System.Drawing.Size(135, 32);
+            this.FinalTemp_Label.TabIndex = 14;
+            this.FinalTemp_Label.Text = "Final Temp:";
+            // 
+            // FinalFlow_Label
+            // 
+            this.FinalFlow_Label.AutoSize = true;
+            this.FinalFlow_Label.Location = new System.Drawing.Point(1326, 483);
+            this.FinalFlow_Label.Name = "FinalFlow_Label";
+            this.FinalFlow_Label.Size = new System.Drawing.Size(125, 32);
+            this.FinalFlow_Label.TabIndex = 15;
+            this.FinalFlow_Label.Text = "Final Flow:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1613, 724);
+            this.Controls.Add(this.FinalFlow_Label);
+            this.Controls.Add(this.FinalTemp_Label);
+            this.Controls.Add(this.FinalH2O_Label);
+            this.Controls.Add(this.FinalCO2_Label);
+            this.Controls.Add(this.PreviousValve_Label);
+            this.Controls.Add(this.CurrentFileLocation_Label);
+            this.Controls.Add(this.CurrentFlow_Label);
+            this.Controls.Add(this.CurrentTemp_Label);
+            this.Controls.Add(this.CurrentH2O_Label);
+            this.Controls.Add(this.CurrentCO2_Label);
+            this.Controls.Add(this.ActiveChamber_Label);
+            this.Controls.Add(this.Stop_Button);
+            this.Controls.Add(this.ValveChecklist_Label);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.StartNewExperiment_Button);
+            this.Controls.Add(this.CheckAllValves_Button);
+            this.Name = "Form1";
+            this.Text = "ProResp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button CheckAllValves_Button;
+        private Button StartNewExperiment_Button;
+        private CheckedListBox checkedListBox1;
+        private Label ValveChecklist_Label;
+        private Button Stop_Button;
+        private Label ActiveChamber_Label;
+        private Label CurrentCO2_Label;
+        private Label CurrentH2O_Label;
+        private Label CurrentTemp_Label;
+        private Label CurrentFlow_Label;
+        private Label CurrentFileLocation_Label;
+        private Label PreviousValve_Label;
+        private Label FinalCO2_Label;
+        private Label FinalH2O_Label;
+        private Label FinalTemp_Label;
+        private Label FinalFlow_Label;
     }
 }
