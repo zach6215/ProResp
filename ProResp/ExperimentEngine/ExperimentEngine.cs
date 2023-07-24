c5af8ec863f1478d933c263d442d5b26e68467d6
Zachary <zachary.c.hall@wsu.edu>
2023-07-23 18:45:46 -0700

    Changed valvesList from List type to LinkedList type.

--- ProResp/ExperimentEngine/ExperimentEngine.cs
+++ ProResp/ExperimentEngine/ExperimentEngine.cs
@@ -2,13 +2,14 @@
 {
     using System;
     using System.Timers;
+    using System.Collections.Generic;
     using LI7000Connection;
 
 
     public class ExperimentEngine
     {
-        private List<Valve> valvesList;
-        private Valve activeValve;
+        private LinkedList<Valve> valvesList;
+        private LinkedListNode<Valve> currentNode;
         private int msValveSwitchTime; // Milliseconds until valve switch
         private int msValveDataTime;    //Milliseconds until data is updated
         private Timer? valveDataTimer;
@@ -25,17 +26,6 @@
         public string DateTimeHeader { get { return dateTimeHeader; } private set { dateTimeHeader = value; } }
         public string LI7000DataHeader { get { return lI7000DataHeader; } private set { lI7000DataHeader = value; } }
         public double DaysSinceStart { get { return (DateTime.Now.Date - startDateTime.Date).TotalDays; } }
-        
-        public List<Valve> ValvesList 
-        { 
-          get { return new List<Valve>(valvesList); }
-          private set { valvesList = value; }
-        }
-        public Valve ActiveValve
-        {
-            get { return this.activeValve; }
-            private set { this.activeValve = value; }
-        }
 
         //Abhi: This constructor can be used to create experiment for Check Valves Button
         public ExperimentEngine(List<string> argActiveValves, int argMsValveSwitchTime, Timer argValveSwitchTimer)
@@ -45,40 +35,41 @@
 
         public ExperimentEngine(List<int> argActiveValves, int argMsValveDataTime, int argMsValveSwitchTime, Timer argValveDataTimer, Timer argValveSwitchTimer)
         {
-            this.valvesList = new List<Valve>();
-            //this.LI7000 = new LI7000Connection();
+            this.valvesList = new LinkedList<Valve>();
+            this.LI7000 = new LI7000Connection();
 
-            //this.LI7000DataHeader = this.LI7000.DataHeader;
-            //this.DateTimeHeader = "Date (mm/dd/yyyy) \t Time (hh:mm)";
+            this.LI7000DataHeader = this.LI7000.DataHeader;
+            this.DateTimeHeader = "Date (mm/dd/yyyy) \t Time (hh:mm)";
 
-            //string[] units = LI7000DataHeader.Split('\t');
+            string[] units = LI7000DataHeader.Split('\t');
 
             foreach (int valveNum in argActiveValves)
             {
                 Valve newValve = new Valve(valveNum);
 
-                //for (int i = 0; i < units.Length; i++)
-                //{
-                //    if (units[i].Contains("CO2"))
-                //    {
-                //        units[i] = units[i].Substring(units[i].IndexOf(' ') + 1);
-                //        newValve.CO2Units = units[i];
-                //    }
-                //    else if (units[i].Contains("H2O"))
-                //    {
-                //        units[i] = units[i].Substring(units[i].IndexOf(' ') + 1);
-                //        newValve.H2OUnits = units[i];
-                //    }
-                //    else if (units[i].Contains('T'))
-                //    {
-                //        units[i] = units[i].Substring(units[i].IndexOf(' ') + 1);
-                //        newValve.TemperatureUnits = units[i];
-                //    }
-                //}
-                this.valvesList.Add(newValve);
+                for (int i = 0; i < units.Length; i++)
+                {
+                    if (units[i].Contains("CO2"))
+                    {
+                        units[i] = units[i].Substring(units[i].IndexOf(' ') + 1);
+                        newValve.CO2Units = units[i];
+                    }
+                    else if (units[i].Contains("H2O"))
+                    {
+                        units[i] = units[i].Substring(units[i].IndexOf(' ') + 1);
+                        newValve.H2OUnits = units[i];
+                    }
+                    else if (units[i].Contains('T'))
+                    {
+                        units[i] = units[i].Substring(units[i].IndexOf(' ') + 1);
+                        newValve.TemperatureUnits = units[i];
+                    }
+                }
+                LinkedListNode<Valve> newNode = new LinkedListNode<Valve>(newValve); 
+                this.valvesList.AddLast(newNode);
             }
 
-            this.activeValve = valvesList[0];
+            this.currentNode = valvesList.First;
 
 
             this.msValveSwitchTime = argMsValveSwitchTime;
@@ -111,7 +102,7 @@
 
         private void UpdateValveValue(Object source, ElapsedEventArgs e)
         {
-            this.activeValve.MeasurementDateTime = DateTime.Now;
+            this.currentNode.Value.MeasurementDateTime = DateTime.Now;
             string? response = LI7000.Poll();
             string newData = string.Empty;
 
@@ -127,24 +118,23 @@
                     switch (headers[i][0])
                     {
                         case 'C':
-                            this.activeValve.CO2 = double.Parse(data[i]);
+                            this.currentNode.Value.CO2 = double.Parse(data[i]);
                             break;
                         case 'H':
-                            this.activeValve.H2O = double.Parse(data[i]);
+                            this.currentNode.Value.H2O = double.Parse(data[i]);
                             break;
                         case 'T':
-                            this.activeValve.Temperature = double.Parse(data[i]);
+                            this.currentNode.Value.Temperature = double.Parse(data[i]);
                             break;
                     }
                 }
-                this.DataUpdated.Invoke(this, new DataUpdateEventArgs(this.activeValve));
+                this.DataUpdated.Invoke(this, new DataUpdateEventArgs(this.currentNode.Value));
             }
         }
 
         //Abhi: Add code to switch valves here. In the end it should invoke this.ValveSwitched event.
         private void SwitchValves(Object source, ElapsedEventArgs e)
         {
-
         }
 
         public void Stop()
